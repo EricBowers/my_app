0ced634424c9ddacbc9b97dde589a1327ee8be75
Eric Bowers <ericbowers62@gmail.com>
2015-06-09 14:16:36 -0500

    Add image upload with Paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,19 @@
+require 'paperclip/media_type_spoof_detector'
+
+module Paperclip
+  class MediaTypeSpoofDetector
+    def spoofed?
+      false
+    end
+  end
+end
+
 class Pin < ActiveRecord::Base
+	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.1-Q16'
 	belongs_to :user
+
+	has_attached_file :image, :styles => { :large => "1000x1000", :medium => "500x500>", :thumb => "300x300>" }
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+
 end
